72557d74823464c35566e1bb916ee4b785a966e2
Youchen Lee <copyleft@utcr.org>
2014-11-11 17:44:55 +0800

    fix autoload

--- bootstrap.php
+++ bootstrap.php
@@ -5,11 +5,13 @@ define('BASEDIR', __DIR__);
 class GeneralException extends Exception { }
 
 spl_autoload_register(function ($class) {
-	$filename = realpath(BASEDIR . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . strtolower(str_replace('_', DIRECTORY_SEPARATOR, $class)) . '.php');
-	if ($filename)
+	$filename = realpath(BASEDIR . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR . $class . '.php');
+
+	if ($filename) {
 		include($filename);
-	else
+	} else {
 		throw new GeneralException('Can not find file ' . $class);
+	}
 });
 
 
