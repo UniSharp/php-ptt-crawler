0041dba62e0847eddf3b16dc5ec9c1ad7c2d919b
Youchen Lee <copyleft@utcr.org>
2014-11-11 18:15:18 +0800

    support long opts & enhance state judgement

--- classes/PttCrawler.php
+++ classes/PttCrawler.php
@@ -7,6 +7,9 @@ class PttCrawler
 	private $board_name = null; // 版名
 	private $config = array(); // 設定參數陣列
 
+	const STATE_DUPLICATED = 0x01;
+	const STATE_DATE_REACHED = 0x02;
+
 	public function __construct($storage, $board_name)
 	{
 		date_default_timezone_set("Asia/Taipei");
@@ -27,9 +30,10 @@ class PttCrawler
 		// 連線送出timeout
 		$this->config["timeout"] = (!isset($config["timeout"])) ? 10 : $config["timeout"];
 		// 抓取文章的最後日期
-		$this->config["last_date"] = (!isset($config["last_date"])) ? date("Y-m-d") : $config["last_date"];
+		$this->config["stop-date"] = (!isset($config["stop-date"])) ? date("Y-m-d") : $config["stop-date"];
 		// 設定是否只抓到上次的最後一篇
-		$this->config["is_last_date"] = (!isset($config["is_last_date"])) ? true : $config["is_last_date"];
+		$this->config["stop-on-duplicate"] = (!isset($config["stop-on-duplicate"])) ? true : $config["stop-on-duplicate"];
+		var_dump($this->config);
 	}
 
 	// 供外部程式呼叫執行
@@ -42,14 +46,14 @@ class PttCrawler
 	// 主程式邏輯
 	private function main()
 	{
-		$is_to_date = false;
-		$is_repeated = false;
+		$state = 0;
+		$is_stop = false;
 		// 取得總頁數
 		$last_page = $this->page_count();
 
 		for ($i = $last_page; $i >= 1; $i--) {
 			// 檢查爬蟲是否該繼續爬資料
-			if ($is_to_date || $is_repeated) break;
+			if ($is_stop) break;
 			// 取得每頁文章基本資料
 			$current_page = $this->fetch_page($i);
 
@@ -65,15 +69,17 @@ class PttCrawler
 				if ($this->storage->GetArticleByUrl($item["url"])) {
 					$this->error_output("notice! article: " . $item["url"] . " has been in database \n");
 					// 檢查是否抓到上次最後一篇
-					if ($this->config["is_last_date"]) {
-						$is_repeated = true;
+					if ($this->config["stop-on-duplicate"]) {
+						$is_stop = true;
+						$state |= self::STATE_DUPLICATED;
 					}
 					continue;
 				}
 				// 略過已到設定日期文章
 				if ($this->is_date_over($item["date"])) {
-					$this->error_output("notice! article: " . $item["url"] . " is earlier than " . $this->config["last_date"] . " \n");
-					$is_to_date = true;
+					$this->error_output("notice! article: " . $item["url"] . " is earlier than " . $this->config["stop-date"] . " \n");
+					$is_stop = true;
+					$state |= self::STATE_DATE_REACHED;
 					continue;
 				}
 				// 存入要抓取詳細資料的article陣列
@@ -112,13 +118,16 @@ class PttCrawler
 			}
 		}
 		// 檢測文章是否到期
-		if ($is_to_date) {
-			$this->error_output("articles are earlier than " . $this->config["last_date"] . ", stop fetching... \n");
-		// 檢查是否已經抓到上次的最後一篇
-		} else if ($is_repeated) {
-			$this->error_output("no more lastest pages! stop fetching... \n");
+		if ($state & self::STATE_DATE_REACHED) {
+			$this->error_output("Stop fetching cause the article date are older than " . $this->config["stop-date"] . "\n");
 		}
-		$this->error_output("fetch finished! \n");
+
+		// 檢查是否已重複
+		if ($state & self::STATE_DUPLICATED) {
+			$this->error_output("Stop fetching cause the articles are duplicated. \n");
+		}
+
+		$this->error_output("Fetch finished! \n");
 		return true;
 	}
 
@@ -250,7 +259,7 @@ class PttCrawler
 
 	private function is_date_over($article_date)
 	{
-		return (strtotime(date($article_date)) <= strtotime('-1 day', strtotime($this->config["last_date"]))) ? true : false;
+		return (strtotime(date($article_date)) <= strtotime('-1 day', strtotime($this->config["stop-date"]))) ? true : false;
 	}
 
 	private function init_opts()
--- crawler.php
+++ crawler.php
@@ -2,17 +2,51 @@
 require 'bootstrap.php';
 
 $board_name = null;
-if (isset($argv[1])) {
-	$board_name = $argv[1];
-} else {
+$shortopts  = "";
+$longopts  = array(
+	"board:",
+	"sleep-between-list:",
+	"sleep-between-article:",
+	"sleep-between-retry:",
+	"timeout:",
+	"stop-date:",
+	"stop-on-duplicate",
+	"debug",
+	"help"
+);
+
+$options = getopt($shortopts, $longopts);
+
+
+if (isset($options['help']) || !array_key_exists('board', $options)) {
 	$fe = fopen('php://stderr', 'w');
-	fwrite($fe, "usage: php crawler.php {Board Name} --last=true\n");
-	fclose($fe);
+	$help_msg = <<<EOF
+Usage: php crawler.php --board=<board name> {options}
+  --sleep-between-list=INT    : Seconds to sleep between fetching different index pages.
+  --sleep-between-article=INT : Seconds to sleep between fetching articles.
+  --sleep-between-retry=INT   : Seconds to sleep when error occurrs.
+  --timeout=INT               : Seconds of the http timeout.
+  --stop-date=DATE            : Stop the program when articles older than the specific date. (default: today)
+  --stop-on-duplicate         : Stop crawling when articles are duplicated. (default: true)
+  --debug                     : Enable debug.
+EOF;
+
+	fwrite($fe, "$help_msg\n");
 	exit(1);
 }
 
+$board_name = $options['board'];
+
 $db = new RDB();
 $PttCrawler = new PttCrawler($db, $board_name);
-$PttCrawler->set_config(array(
-	"last_date" => "2014-11-11"));
+$PttCrawler->set_config(
+	array(
+		"list_sleep" => isset($options['sleep-between-list']) ? $options['sleep-between-list'] : null,
+		"article_sleep" => isset($options['sleep-between-article']) ? $options['sleep-between-article'] : null,
+		"error_sleep" => isset($options['sleep-between-retry']) ? $options['sleep-between-retry'] : null,
+		"timeout" => isset($options['timeout']) ? $options['timeout'] : null,
+		"stop-date" => isset($options['stop-date']) ? $options['stop-date'] : null,
+		"stop-on-duplicate" => isset($options['stop-on-duplicate']) ? true : null,
+	)
+);
 $PttCrawler->run();
