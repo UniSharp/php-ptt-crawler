8e0cdb83fd7039c6c9f1b9e3fd9ab24aaeef363d
Youchen Lee <copyleft@utcr.org>
2014-11-11 17:48:44 +0800

    refector db reconnection script

--- classes/Database.php
+++ classes/Database.php
@@ -27,20 +27,18 @@ class Database
 			$this->db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
             $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch (PDOException $e) {
-			exit("database connection error");
+			exit("Database connection error\n");
 		}
 	}
 
 	public function reconnectPDO()
 	{
 		$this->db = null;
-		$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
-		try {
-			$this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS, $options);
-			$this->db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		} catch (PDOException $e) {
-			exit("database connection error");
-		}
+		$this->openPDO();
+	}
+
+	public function __destruct()
+	{
+		$this->db = null;
 	}
 }
